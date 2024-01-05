6438fe4e8d76cd71c2a292662f529b2fa318349f
nuri <nuriozbey@gmail.com>
2024-01-05 10:24:12 +0300

    styles new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,90 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
-    new SweetScroll({}),
-        particlesJS("particles-js",
-            {
-                particles: {
-                    number: {
-                        value: 30,
-                        density: {
-                            enable: !0,
-                            value_area: 800
-                        }
-                    },
-                    color: {
-                        value: "#ffffff"
-                    },
-                    shape: {
-                        type: "polygon",
-                        stroke: {
-                            width: 0,
-                            color: "#000000"
-                        },
-                        polygon: {
-                            nb_sides: 5
-                        },
-                        image: {
-                            src: "img/github.svg",
-                            width: 100,
-                            height: 100
-                        }
-                    },
-                    opacity: {
-                        value: .5,
-                        random: !1,
-                        anim: {
-                            enable: !1,
-                            speed: 1,
-                            opacity_min: .1,
-                            sync: !1
-                        }
-                    },
-                    size: {
-                        value: 3,
-                        random: !0,
-                        anim: {
-                            enable: !1,
-                            speed: 19.18081918081918,
-                            size_min: .1,
-                            sync: !1
-                        }
-                    },
-                    line_linked: {
-                        enable: !0,
-                        distance: 150,
-                        color: "#ffffff",
-                        opacity: .4,
-                        width: 1
-                    },
-                    move: {
-                        enable: !0,
-                        speed: 4,
-                        direction: "none",
-                        random: !0,
-                        straight: !1,
-                        out_mode: "out",
-                        bounce: !1,
-                        attract: {
-                            enable: !1,
-                            rotateX: 600,
-                            rotateY: 1200
-                        }
-                    },
-                    nb: 80
-                },
-                interactivity: {
-                    detect_on: "canvas",
-                    events: {
-                        onhover: { enable: !1, mode: "grab" },
-                        onclick: { enable: !0, mode: "push" }, resize: !0
-                    },
-                    modes: {
-                        grab: { distance: 400, line_linked: { opacity: 1 } },
-                        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-                        repulse: { distance: 200, duration: .4 },
-                        push: { particles_nb: 4 },
-                        remove: { particles_nb: 2 }
-                    }
-                },
-                retina_detect: !0
-            })
+    new SweetScroll({});
+    particlesJS("particles-js", {
+        particles: {
+          number: {
+            value: 180,
+            density: { enable: true, value_area: 1575.4233950374157 }
+          },
+          color: { value: "#fff" },
+          shape: {
+            type: "circle",
+            stroke: { width: 0, color: "#000000" },
+            polygon: { nb_sides: 5 },
+            image: { src: "img/github.svg", width: 100, height: 100 }
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+          },
+          size: {
+            value: 6,
+            random: true,
+            anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+          },
+          line_linked: {
+            enable: false,
+            distance: 500,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 2
+          },
+          move: {
+            enable: true,
+            speed: 6,
+            direction: "bottom",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: { enable: false, rotateX: 600, rotateY: 1200 }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: { enable: true, mode: "bubble" },
+            onclick: { enable: true, mode: "repulse" },
+            resize: true
+          },
+          modes: {
+            grab: { distance: 400, line_linked: { opacity: 0.5 } },
+            bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
+            repulse: { distance: 200, duration: 0.4 },
+            push: { particles_nb: 4 },
+            remove: { particles_nb: 2 }
+          }
+        },
+        retina_detect: true
+      });
+      var count_particles, stats, update;
+      stats = new Stats();
+      stats.setMode(0);
+      stats.domElement.style.position = "absolute";
+      stats.domElement.style.left = "0px";
+      stats.domElement.style.top = "0px";
+      document.body.appendChild(stats.domElement);
+      count_particles = document.querySelector(".js-count-particles");
+      update = function () {
+        stats.begin();
+        stats.end();
+        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+          count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+        }
+        requestAnimationFrame(update);
+      };
+      requestAnimationFrame(update);      
 }, !1);
